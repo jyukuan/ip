b72b2ab997841e46188577c5a2f5e16b491d2b3a
yukuan <e1249416@u.nus.edu>
2025-02-04 23:24:16 +0800

    Week 3 task 3 : Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,35 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String[] list = new String[100];
-    private static int listCount = 0;
+    //new class : task for store name and done or not
+    private static class Task {
+        String description;
+        boolean isDone;
 
-    private static String copy(String input) {
-        return input;
+        Task(String name) {
+            this.description = name;
+            this.isDone = false;
+        }
+
+        String getStatus() {
+            if (isDone) {
+                return "[X]";
+            } else {
+                return "[ ]";
+            }
+        }
+
+        void markDone() {
+            this.isDone = true;
+        }
+
+        void markNotDone() {
+            this.isDone = false;
+        }
     }
+
+    private static Task[] list = new Task[100];
+    private static int listCount = 0;
 
     private static void printIt(String message) {
         System.out.println("____________________________________________________________");
@@ -15,9 +38,9 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
-    private static void addToList (String input) {
+    private static void addToList(String input) {
         if (listCount < list.length) {
-            list[listCount] = input;
+            list[listCount] = new Task(input);
             listCount++;
             printIt("added: " + input);
         } else {
@@ -27,10 +50,41 @@ public class Duke {
 
     private static void printList() {
         System.out.println("____________________________________________________________");
-        for (int i = 0; i < listCount; i ++) {
-            System.out.println((i + 1) + ". " + list[i]);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < listCount; i++) {
+            System.out.println((i + 1) + "." + list[i].getStatus() + " " + list[i].description);
         }
         System.out.println("____________________________________________________________");
+    }
+
+    private static void markTaskAsDone(int index) {
+        if (index >= 0 && index < listCount) {
+            list[index].markDone();
+            printIt("Nice! I've marked this task as done:\n  " + list[index].getStatus() + " " + list[index].description);
+        } else {
+            printIt("Invalid!");
+        }
+    }
+
+    private static void markTaskAsNotDone(int index) {
+        if (index >= 0 && index < listCount) {
+            list[index].markNotDone();
+            printIt("OK, I've marked this task as not done yet:\n  " + list[index].getStatus() + " " + list[index].description);
+        } else {
+            printIt("Invalid!");
+        }
+    }
+
+    private static boolean isValidNumber(String str) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -44,19 +98,37 @@ public class Duke {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase ("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (input.equalsIgnoreCase ("list")) {
+            } else if (input.equalsIgnoreCase("list")) {
                 printList();
+            } else if (input.length() > 5 && input.substring(0, 5).equals("mark ")) {
+                String numberPart = input.substring(5);
+                if (isValidNumber(numberPart)) {
+                    int index = Integer.parseInt(numberPart) - 1;
+                    markTaskAsDone(index);
+                } else {
+                    printIt("Invalid task number!");
+                }
+            } else if (input.length() > 7 && input.substring(0, 7).equals("unmark ")) {
+                String numberPart = input.substring(7);
+                if (isValidNumber(numberPart)) {
+                    int index = Integer.parseInt(numberPart) - 1;
+                    markTaskAsNotDone(index);
+                } else {
+                    printIt("Invalid!");
+                }
             } else {
                 addToList(input);
             }
-
-            printIt(copy(input));
         }
 
+        scanner.close();
     }
+
 }
+
+
