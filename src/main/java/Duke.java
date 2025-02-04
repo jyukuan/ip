b51a42ea19d61d75e819393446e19f5ba09a8749
yukuan <e1249416@u.nus.edu>
2025-02-04 22:39:13 +0800

    Week 3 task 2 : Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,34 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static String[] list = new String[100];
+    private static int listCount = 0;
+
     private static String copy(String input) {
         return input;
     }
 
-    private static void printit(String message) {
+    private static void printIt(String message) {
         System.out.println("____________________________________________________________");
         System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void addToList (String input) {
+        if (listCount < list.length) {
+            list[listCount] = input;
+            listCount++;
+            printIt("added: " + input);
+        } else {
+            printIt("Task list is full!");
+        }
+    }
+
+    private static void printList() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < listCount; i ++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
         System.out.println("____________________________________________________________");
     }
 
@@ -28,9 +49,13 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase ("list")) {
+                printList();
+            } else {
+                addToList(input);
             }
 
-            printit(copy(input));
+            printIt(copy(input));
         }
 
     }
