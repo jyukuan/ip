d80e8a7b7b473b31a3067fc64a686c1556a323c7
yukuan <e1249416@u.nus.edu>
2025-02-21 00:22:51 +0800

    UI

---
+++ src/main/java/Ui.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showWelcome() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm LM.");
+        System.out.println("How can I HELP you today? :)");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showGoodbye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye! Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showError(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showTaskAdded(Task task, int taskCount) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Task added:\n  " + task + "\nTotal tasks: " + taskCount);
+        System.out.println("____________________________________________________________");
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
